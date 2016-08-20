2284295e46ccaad459d14172d33e8dfc345d8f94
meganii <yuhei24@gmail.com>
2016-08-20 23:03:49 +0900

    clean indent

--- src/ElectronFlickr.js
+++ src/ElectronFlickr.js
@@ -4,133 +4,133 @@ const utils = new Utils();
 import request from 'superagent';
 
 export default class ElectronFlickr {
-     constructor(options) {
-        this.options = options;
-    }
+  constructor(options) {
+    this.options = options;
+  }
 
-    getRequestTokenURL() {
-        this.options = utils.setAuthVals(this.options);
-        var url = "https://www.flickr.com/services/oauth/request_token";
-        var queryArguments = {
-            oauth_callback:         this.options.callback,
-            oauth_consumer_key:     this.options.api_key,
-            oauth_nonce:            this.options.oauth_nonce,
-            oauth_timestamp:        this.options.oauth_timestamp,
-            oauth_signature_method: "HMAC-SHA1",
-            oauth_version:          "1.0",
-        };
+  getRequestTokenURL() {
+    this.options = utils.setAuthVals(this.options);
+    var url = "https://www.flickr.com/services/oauth/request_token";
+    var queryArguments = {
+      oauth_callback:         this.options.callback,
+      oauth_consumer_key:     this.options.api_key,
+      oauth_nonce:            this.options.oauth_nonce,
+      oauth_timestamp:        this.options.oauth_timestamp,
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_version:          "1.0",
+    };
 
-        const queryString = utils.formQueryString(queryArguments);
-        const data = utils.formBaseString("GET", url, queryString);
-        const signature = utils.sign(data, this.options.secret, this.options.access_token_secret);
-        const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
-        return flickrURL;
-    }
+    const queryString = utils.formQueryString(queryArguments);
+    const data = utils.formBaseString("GET", url, queryString);
+    const signature = utils.sign(data, this.options.secret, this.options.access_token_secret);
+    const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
+    return flickrURL;
+  }
 
-    getAuthToken(url, next) {
-        request
-            .get(url)
-            .end( (err, res) => {
-                const response = utils.parseRestResponse(res.text)
-                this.options.oauth_token = response.oauth_token;
-                this.options.oauth_token_secret = response.oauth_token_secret;
-                next(err, res);
-            });
-    }
+  getAuthToken(url, next) {
+    request
+    .get(url)
+    .end( (err, res) => {
+      const response = utils.parseRestResponse(res.text)
+      this.options.oauth_token = response.oauth_token;
+      this.options.oauth_token_secret = response.oauth_token_secret;
+      next(err, res);
+    });
+  }
 
-    getAuthURL() {
-        const authURL = "https://www.flickr.com/services/oauth/authorize";
-        return authURL + "?oauth_token=" + this.options.oauth_token + "&perms=" + this.options.permissions;
-    }
+  getAuthURL() {
+    const authURL = "https://www.flickr.com/services/oauth/authorize";
+    return authURL + "?oauth_token=" + this.options.oauth_token + "&perms=" + this.options.permissions;
+  }
 
-    setOAuthVerifier(oauthVerifier) {
-        this.options.oauth_verifier = oauthVerifier;
-    }
+  setOAuthVerifier(oauthVerifier) {
+    this.options.oauth_verifier = oauthVerifier;
+  }
 
-    getAccessToken(next) {
-        this.options = utils.setAuthVals(this.options);
-        var queryArguments = {
-                oauth_consumer_key:     this.options.api_key,
-                oauth_nonce:            this.options.oauth_nonce,
-                oauth_signature_method: "HMAC-SHA1",
-                oauth_timestamp:        this.options.oauth_timestamp,
-                oauth_verifier:         this.options.oauth_verifier,
-                oauth_token:            this.options.oauth_token,
-            };
-        const queryString = utils.formQueryString(queryArguments);
-        const url = "https://www.flickr.com/services/oauth/access_token";
-        const data = utils.formBaseString("GET", url, queryString);
-        const signature = utils.sign(data, this.options.secret, this.options.oauth_token_secret);
-        const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
+  getAccessToken(next) {
+    this.options = utils.setAuthVals(this.options);
+    var queryArguments = {
+      oauth_consumer_key:     this.options.api_key,
+      oauth_nonce:            this.options.oauth_nonce,
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_timestamp:        this.options.oauth_timestamp,
+      oauth_verifier:         this.options.oauth_verifier,
+      oauth_token:            this.options.oauth_token,
+    };
+    const queryString = utils.formQueryString(queryArguments);
+    const url = "https://www.flickr.com/services/oauth/access_token";
+    const data = utils.formBaseString("GET", url, queryString);
+    const signature = utils.sign(data, this.options.secret, this.options.oauth_token_secret);
+    const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
 
-        request
-            .get(flickrURL)
-            .end( (err, res) => {
-                const response = utils.parseRestResponse(res.text)
-                this.setAccessToken(response.oauth_token, response.oauth_token_secret);
-                next(err, res);
-            });
-    }
+    request
+    .get(flickrURL)
+    .end( (err, res) => {
+      const response = utils.parseRestResponse(res.text)
+      this.setAccessToken(response.oauth_token, response.oauth_token_secret);
+      next(err, res);
+    });
+  }
 
-    setAccessToken(accessToken, accessTokenSecret) {
-        this.options.access_token = accessToken;
-        this.options.access_token_secret = accessTokenSecret;
-    }
+  setAccessToken(accessToken, accessTokenSecret) {
+    this.options.access_token = accessToken;
+    this.options.access_token_secret = accessTokenSecret;
+  }
 
-    login(next) {
-        this.options = utils.setAuthVals(this.options);
-        var queryArguments = {
-            method:                 "flickr.test.login",
-            oauth_consumer_key:     this.options.api_key,
-            oauth_nonce:            this.options.oauth_nonce,
-            oauth_timestamp:        this.options.oauth_timestamp,
-            oauth_signature_method: "HMAC-SHA1",
-            oauth_version:          "1.0",
-            oauth_token:            this.options.access_token,
-        };
+  login(next) {
+    this.options = utils.setAuthVals(this.options);
+    var queryArguments = {
+      method:                 "flickr.test.login",
+      oauth_consumer_key:     this.options.api_key,
+      oauth_nonce:            this.options.oauth_nonce,
+      oauth_timestamp:        this.options.oauth_timestamp,
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_version:          "1.0",
+      oauth_token:            this.options.access_token,
+    };
 
-        const queryString = utils.formQueryString(queryArguments);
-        const url = "https://api.flickr.com/services/rest";
-        const data = utils.formBaseString("GET", url, queryString);
-        const signature = utils.sign(data, this.options.secret, this.options.access_token_secret);
-        const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
+    const queryString = utils.formQueryString(queryArguments);
+    const url = "https://api.flickr.com/services/rest";
+    const data = utils.formBaseString("GET", url, queryString);
+    const signature = utils.sign(data, this.options.secret, this.options.access_token_secret);
+    const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
 
-        request
-            .get(flickrURL)
-            .end( (err, res) => {
-                next(err, res);
-            });
-    }
+    request
+    .get(flickrURL)
+    .end( (err, res) => {
+      next(err, res);
+    });
+  }
 
-    upload(photoPath, next) {
-        this.options = utils.setAuthVals(this.options);
-        var queryArguments = {
-            oauth_consumer_key:     this.options.api_key,
-            oauth_nonce:            this.options.oauth_nonce,
-            oauth_timestamp:        this.options.oauth_timestamp,
-            oauth_signature_method: "HMAC-SHA1",
-            oauth_version:          "1.0",
-            oauth_token:            this.options.access_token,
+  upload(photoPath, next) {
+    this.options = utils.setAuthVals(this.options);
+    var queryArguments = {
+      oauth_consumer_key:     this.options.api_key,
+      oauth_nonce:            this.options.oauth_nonce,
+      oauth_timestamp:        this.options.oauth_timestamp,
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_version:          "1.0",
+      oauth_token:            this.options.access_token,
 
-        };
-        const url = "https://up.flickr.com/services/upload/";
-        const queryString = utils.formQueryString(queryArguments);
-        const data = utils.formBaseString("POST", url, queryString);
-        const signature = utils.sign(data, this.options.secret, this.options.access_token_secret);
-        const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
+    };
+    const url = "https://up.flickr.com/services/upload/";
+    const queryString = utils.formQueryString(queryArguments);
+    const data = utils.formBaseString("POST", url, queryString);
+    const signature = utils.sign(data, this.options.secret, this.options.access_token_secret);
+    const flickrURL = url + "?" + queryString + "&oauth_signature=" + signature;
 
-        request
-            .post(flickrURL)
-            .field('oauth_consumer_key',     queryArguments.oauth_consumer_key)
-            .field('oauth_nonce',            queryArguments.oauth_nonce)
-            .field('oauth_timestamp',        queryArguments.oauth_timestamp)
-            .field('oauth_signature_method', queryArguments.oauth_signature_method)
-            .field('oauth_version',          queryArguments.oauth_version)
-            .field('oauth_token',            queryArguments.oauth_token)
-            .field('oauth_signature',        signature)
-            .attach('photo', photoPath)
-            .end( (err, res) => {
-                next(err, res);
-            });
-    }
+    request
+    .post(flickrURL)
+    .field('oauth_consumer_key',     queryArguments.oauth_consumer_key)
+    .field('oauth_nonce',            queryArguments.oauth_nonce)
+    .field('oauth_timestamp',        queryArguments.oauth_timestamp)
+    .field('oauth_signature_method', queryArguments.oauth_signature_method)
+    .field('oauth_version',          queryArguments.oauth_version)
+    .field('oauth_token',            queryArguments.oauth_token)
+    .field('oauth_signature',        signature)
+    .attach('photo', photoPath)
+    .end( (err, res) => {
+      next(err, res);
+    });
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -4,57 +4,57 @@ const crypto = require("crypto");
 
 export default class Utils {
 
-    sign(data, key, secret) {
-        const hmacKey = key + "&" + (secret ? secret : '');
-        let hmac = crypto.createHmac("SHA1", hmacKey);
-        hmac.update(data);
-        const digest = hmac.digest("base64");
-        return encodeURIComponent(digest);
-    }
+  sign(data, key, secret) {
+    const hmacKey = key + "&" + (secret ? secret : '');
+    let hmac = crypto.createHmac("SHA1", hmacKey);
+    hmac.update(data);
+    const digest = hmac.digest("base64");
+    return encodeURIComponent(digest);
+  }
+  
+  encodeRFC5987ValueChars(str) {
+    return encodeURIComponent(str).
+    replace(/['()!]/g, escape).
+    replace(/\*/g, '%2A').
+    replace(/%(?:7C|60|5E)/g, unescape);
+  }
 
-    encodeRFC5987ValueChars(str) {
-        return encodeURIComponent(str).
-            replace(/['()!]/g, escape).
-            replace(/\*/g, '%2A').
-            replace(/%(?:7C|60|5E)/g, unescape);
-    }
+  formQueryString(queryArguments) {
+    return Object.keys(queryArguments)
+    .sort()
+    .map( (key) => key + "=" + this.encodeRFC5987ValueChars(queryArguments[key]))
+    .join("&");
+  }
 
-    formQueryString(queryArguments) {
-        return Object.keys(queryArguments)
-                .sort()
-                .map( (key) => key + "=" + this.encodeRFC5987ValueChars(queryArguments[key]))
-                .join("&");
-    }
+  /**
+  * Turn a url + query string into a Flickr API "base string".
+  */
+  formBaseString(verb, url, queryString) {
+    return [verb, encodeURIComponent(url), encodeURIComponent(queryString)].join("&");
+  }
 
-    /**
-     * Turn a url + query string into a Flickr API "base string".
-     */
-    formBaseString(verb, url, queryString) {
-      return [verb, encodeURIComponent(url), encodeURIComponent(queryString)].join("&");
-    }
+  setAuthVals(options) {
+    let val = Object.assign({}, options);
+    const timestamp = "" + Date.now();
+    const md5 = crypto.createHash("md5").update(timestamp).digest("hex");
+    const nonce = md5.substring(0, 32);
+    val.oauth_timestamp = timestamp;
+    val.oauth_nonce = nonce;
+    return val;
+  }
 
-    setAuthVals(options) {
-        let val = Object.assign({}, options);
-        const timestamp = "" + Date.now();
-        const md5 = crypto.createHash("md5").update(timestamp).digest("hex");
-        const nonce = md5.substring(0, 32);
-        val.oauth_timestamp = timestamp;
-        val.oauth_nonce = nonce;
-        return val;
-    }
+  parseRestResponse(body) {
+    if (!body) { return false; }
 
-    parseRestResponse(body) {
-        if (!body) { return false; }
+    const constituents = body.split("&");
+    let response = {};
+    let keyval = '';
 
-        const constituents = body.split("&");
-        let response = {};
-        let keyval = '';
+    constituents.forEach((pair) => {
+      keyval = pair.split("=");
+      response[keyval[0]] = decodeURIComponent(keyval[1]);
+    });
 
-        constituents.forEach((pair) => {
-            keyval = pair.split("=");
-            response[keyval[0]] = decodeURIComponent(keyval[1]);
-        });
-
-        return response;
-    }
+    return response;
+  }
 }
